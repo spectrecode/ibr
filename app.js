d000a704c217d14f76978024f26c5b58482e41b6
spectrecode <mchristy.ccb@gmail.com>
2017-01-20 10:12:09 -0500

    cambios e boton y responsive

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-var btnIntro = document.getElementById("intro");
-var videoIntro = document.getElementById("video_intro");
-var introNot = document.getElementById("intro_noticia");	
+// var btnIntro = document.getElementById("intro");
+// var videoIntro = document.getElementById("video_intro");
+// var introNot = document.getElementById("intro_noticia");	
 
-function videoPlay(){
-	btnIntro.remove(this);
-	introNot.classList.remove("hide");
-}
-btnIntro.addEventListener("click", function(){
-	videoPlay();
-})
-videoIntro.addEventListener("click", function(){
-	videoPlay();
-})
+// function videoPlay(){
+// 	btnIntro.remove(this);
+// 	introNot.classList.remove("hide");
+// }
+// btnIntro.addEventListener("click", function(){
+// 	videoPlay();
+// })
+// videoIntro.addEventListener("click", function(){
+// 	videoPlay();
+// })
+$(document).ready(function(){
+    $("#intro,#video_intro").click(function(){
+        $("#intro").hide();
+        $("#intro_noticia").removeClass("hide");
+	});
+    $("#retro").click(function(){
+        $("#intro_noticia").addClass("hide");
+    	$("#intro").show();
+    })
+});
